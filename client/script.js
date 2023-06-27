db4407a9a4ca4513b1f15dc3b5091d645c325b42
jane__ <janecching@gmail.com>
2023-06-26 17:51:43 -0700

    added scenario, level, language option functionality

--- client/script.js
+++ client/script.js
@@ -58,16 +58,61 @@ function chatStripe(isAi, value, uniqueId) {
     )
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var vocabButton = document.getElementById('vocabButton');
+    vocabButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        generateVocabularies();
+    });
+
+    function generateVocabularies() {
+        const language = document.getElementById('language').value;
+        const level = document.getElementById('level').value;
+        const scenario = document.getElementById('scenario').value;
+        const top10Words = {
+            French: {
+                Beginner: {
+                    'Ordering food': ['Bonjour', 'Merci', 'S\'il vous plaît', 'Menu', 'L\'addition', 'Café', 'Pain', 'Fromage', 'Vin', 'Croissant'],
+                    'Checking in a hotel': ['Chambre', 'Réception', 'Clé', 'Réservation', 'Hôtel', 'Lit', 'Douche', 'Serviette', 'Ascenseur', 'Prix'],
+                    'Asking for directions': ['Où est', 'Rue', 'Droite', 'Gauche', 'Quartier', 'Station de métro', 'Arrêt de bus', 'Bâtiment', 'Loin', 'Près'],
+                    'Shopping at a market': ['Marché', 'Fruits', 'Légumes', 'Viande', 'Poisson', 'Pain', 'Fromage', 'Prix', 'Sacs', 'Caisse'],
+                },
+                Intermediate: {
+                    'Ordering food': ['Apéritif', 'Entrée', 'Plat principal', 'Dessert', 'Carte des vins', 'Menu du jour', 'Réservation', 'Serveur', 'Pourboire', 'Service'],
+                    'Checking in a hotel': ['Réceptionniste', 'Confirmation', 'Facture', 'Bagages', 'Service en chambre', 'Climatisation', 'Wifi', 'Piscine', 'Réveil', 'Occupé'],
+                    'Asking for directions': ['Tournez à droite', 'Tournez à gauche', 'Tout droit', 'Au coin de', 'Intersection', 'Passage piéton', 'Feu de signalisation', 'Plan de la ville', 'Arrêt de taxi', 'Se perdre'],
+                    'Shopping at a market': ['Étal', 'Épicerie', 'Commerçant', 'Promotion', 'Panier', 'Balance', 'Monnaie', 'Rayon', 'Ticket de caisse', 'Retourner'],
+                },
+            },
+        };
+
+        const vocabularies = top10Words[language][level][scenario];
+        const resultsDiv = document.getElementById('results');
+        resultsDiv.innerHTML = '<h3>Generated Vocabularies:</h3>';
+        const list = document.createElement('ul');
+        vocabularies.forEach(word => {
+            const listItem = document.createElement('li');
+            listItem.textContent = word;
+            list.appendChild(listItem);
+        });
+        resultsDiv.appendChild(list);
+    }
+});
 
 const handleSubmit = async(e) => {
     e.preventDefault();
-
-    const data = new FormData(form);
+    const userPromptForm = document.getElementById('userPromptForm');
+    const data = new FormData(userPromptForm);
 
     // post user input 
+    const language = document.getElementById('language').value;
+    const level = document.getElementById('level').value;
+    const scenario = document.getElementById('scenario').value;
     const userPrompt = data.get('prompt');
+    console.log(language, level, scenario);
+
     chatContainer.innerHTML += chatStripe(false, userPrompt);
-    form.reset();
+    userPromptForm.reset();
 
     // post bot response
     const uniqueId = generateUniqueId();
@@ -83,9 +128,13 @@ const handleSubmit = async(e) => {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                conversation: userPrompt, // Use "conversation" instead of "prompt" in the request body
+                conversation: userPrompt,
+                language: language,
+                level: level,
+                scenario: scenario,
             }),
         });
+        console.log(userPrompt);
 
         clearInterval(loadInterval);
         messageDiv.innerHTML = ' ';
@@ -110,10 +159,11 @@ const handleSubmit = async(e) => {
 };
 
 
-
 form.addEventListener('submit', handleSubmit);
-form.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        handleSubmit(e);
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault(); // Prevent form submission
+        handleSubmit(event);
     }
 });
--- server/server.js
+++ server/server.js
@@ -14,61 +14,62 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-
+// , language, level, scenario
 async function generateChatResponse(conversation) {
     try {
 
         const response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [
-                { role: "system", content: "You are a french waiter. Use basic french to serve user, provide english translation and correct user's grammar." },
+                { role: "system", content: `You are a french waiter. Use basic french to serve user, provide english translation and correct user's grammar.` },
                 { role: "user", content: conversation },
-            ], //You are a french tutor. Teach user basic phrases and correct user's grammar
+            ],
         });
-        // const response = await openai.createCompletion({
-        //     model: 'text-davinci-003',
-        //     prompt: "The following is a conversation with an french tutor. The tutor speaks in very basic french and provides translation of their response in english, and corrects the student's responses. \n\nHuman: Hello, who are you?",
-        //     temperature: 0,
-        //     max_tokens: 150,
-        //     top_p: 1,
-        //     frequency_penalty: 0,
-        //     presence_penalty: 0.6,
-        //     stop: [' Human:', ' AI:'],
-        // });
-        console.log(response.data.choices)
+        // console.log(response.data.choices)
         const reply = response.data.choices[0].message.content.trim();
         return reply;
     } catch (error) {
-        console.error(error);
+        // console.error(error);
         throw error;
     }
 }
 
 
 app.get('/', async(req, res) => {
+    // console.log('GET request received');
     res.status(200).send({
-        message: 'Hello from your french tutor!'
-    })
+        message: 'Hello from your french tutor!',
+    });
 })
 
 app.post('/', async(req, res) => {
     try {
-        const conversationPrompt = req.body.conversation;
-        console.log(conversationPrompt)
+        // console.log('POST request received');
+        const conversation = req.body.conversation;
+        const language = req.body.language;
+        const level = req.body.level;
+        const scenario = req.body.scenario;
 
+        console.log('Conversation:', conversation);
+        console.log('Language:', language);
+        console.log('Level:', level);
+        console.log('Scenario:', scenario);
 
+        const reply = await generateChatResponse(
+            conversation,
+            // language,
+            // level,
+            // scenario
+        );
 
-
-
-        const reply = await generateChatResponse(conversationPrompt);
+        console.log('Bot reply:', reply);
 
         res.status(200).send({
             bot: reply,
         });
     } catch (error) {
-        console.error(error);
+        // console.error(error);
         res.status(500).send(error || 'Something went wrong');
     }
 });
-
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
